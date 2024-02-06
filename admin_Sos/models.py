a2b1efc56482eceaa5243b1131458a879d53e8f2
ErrajiOussama <oussamabstista02@gmail.com>
2024-02-06 16:11:39 +0100

    Forms_2

--- admin_Sos/models.py
+++ admin_Sos/models.py
@@ -8,7 +8,7 @@ class Collaborateur(models.Model):
     Date_de_naissance = models.DateField()
     Age = models.IntegerField()
     Situation_familiale = models.CharField(max_length=10, null=True,blank=True)
-    Nombre_d_enfants = models.FloatField( null=True,blank=True)
+    Nombre_d_enfants = models.FloatField(null=True,blank=True)
     N_CIN =models.CharField(max_length=12 ,null=True,blank=True)
     N_Passeport=models.CharField(max_length=30 ,null=True,blank=True)
     Nationalité=models.CharField(max_length=20)
@@ -20,10 +20,12 @@ class Collaborateur(models.Model):
     N_CNSS= models.CharField(max_length=20)
     Type_de_contrat= models.CharField(max_length=10)
     Rémunération = models.IntegerField()
-    Prime = models.FloatField( null=True,blank=True)
+    Prime = models.FloatField(null=True,blank=True)
     Poste = models.CharField(max_length=10)
     Date_d_entrée = models.DateField(null=True,blank=True)
     Date_de_Sortie = models.DateField(null=True,blank=True)
+    Taux_Horaire= models.FloatField(null=True,blank=True)
+    Prime_Avancee = models.FloatField(null=True,blank=True)
     
     def __str__(self): 
         return str(self.Nom)
