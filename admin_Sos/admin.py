d6ac35f319a83282f3e8475c585e9191729ef935
ErrajiOussama <oussamabstista02@gmail.com>
2024-03-06 15:26:35 +0100

    modif for 1 Edt

--- admin_Sos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Collaborateur)
-admin.site.register(Salaire)
-admin.site.register(Salaire_admin)
--- admin_Sos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminSosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_Sos'
